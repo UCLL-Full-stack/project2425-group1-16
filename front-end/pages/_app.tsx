c8333348f44a8ce48f5b265cff0c28ade067633b
Kevin Hiers <kevin.hiers@student.ucll.be>
2024-10-31 14:11:56 +0100

    updates

--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import "bootstrap/dist/css/bootstrap.min.css"; //This makes css easier and they used it in lab3 so I just took it in for now
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
