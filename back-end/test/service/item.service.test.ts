d15c9a9dc642105ea0617661d400719d430d88f3
Kevin Hiers <kevin.hiers@student.ucll.be>
2024-11-02 18:44:24 +0100

    Added service tests

---
+++ back-end/test/service/item.service.test.ts
@@ -0,0 +1,102 @@
+import { Category } from "../../model/Category";
+import { Item } from "../../model/Item";
+import { LocationTag } from "../../model/LocationTag";
+import { Profile } from "../../model/Profile";
+import itemDb from "../../repository/item.db";
+import itemService from "../../service/item.service";
+
+
+const validName: string = "Grasmaaier";
+const validName2: string = "Sfeerverlichting";
+
+const validDescription: string = "Dit is een grasmaaier";
+const validDescription2: string = "Dit is wat sfeerverlichting";
+
+const validPrice: number = 10;
+const validPrice2: number = 20;
+
+const validUsername: string = "Michiel05";
+const validUsername2: string = "Kevin04";
+
+const validPassword: string = "K5/#G6es:M(z8,";
+const validPassword2: string = "K4/#G6es:M(z8,";
+
+const validEmail2: string = "Kevin.Hiers@domain.be";
+const validEmail: string = "Michiel.Nijs@domain.be";
+
+const validPhoneNumber: string = "0467725913";
+const validPhoneNumber2: string = "0467724913";
+
+const validLocation: LocationTag = new LocationTag({
+    displayName: "Leuven",
+    latitude: 50.8775,
+    longtitude: 4.70444
+})
+
+const validLocation2: LocationTag = new LocationTag({
+    displayName: "Brussel",
+    latitude: 50.84667,
+    longtitude: 4.35472
+})
+
+const validProfile: Profile = new Profile({username: validUsername, password: validPassword, email: validEmail, phoneNumber: validPhoneNumber, location: validLocation});
+const validProfile2: Profile = new Profile({username: validUsername2, password: validPassword2, email: validEmail2, phoneNumber: validPhoneNumber2, location: validLocation2});
+
+const validCategoryName: string = "Tuingereedschap";
+const validCategoryName2: string = "Grasmaaiers";
+const validCategoryName3: string = "Elektronica";
+const validCategoryName4: string = "Verlichting";
+const validCategoryName5: string = "Decoratie";
+
+const validCategory: Category = new Category({name: validCategoryName, parents: null});
+const validCategory2: Category = new Category({name: validCategoryName3, parents: null});
+const validCategory3: Category = new Category({name: validCategoryName5, parents: null});
+
+const validParents: Category[] = [validCategory];
+const validParents2: Category[] = [validCategory2, validCategory3];
+
+const validCategory4: Category = new Category({name: validCategoryName2, parents: validParents});
+const validCategory5: Category = new Category({name: validCategoryName4, parents: validParents2});
+
+const validCategories: Category[] = [validCategory, validCategory4];
+const validCategories2: Category[] = [validCategory2, validCategory3, validCategory5];
+
+const validItem: Item = new Item({id: 1, name: validName, description: validDescription, price: validPrice, owner: validProfile, location: validLocation, categories: validCategories});
+const validItem2: Item = new Item({id:2, name: validName2, description: validDescription2, price: validPrice2, owner: validProfile2, location: validLocation2, categories: validCategories2});
+
+
+let getItemByIdMock: jest.Mock;
+
+beforeEach(() => {
+    getItemByIdMock = jest.fn();
+});
+
+afterEach(() => {
+    jest.clearAllMocks();
+});
+
+test('given a valid item id, when an item is requested by id, then the item with that id is returned', () => {
+    //given
+    itemDb.getItemById = getItemByIdMock.mockReturnValue(validItem);
+    const itemId = 1;
+    
+    //when
+    const getItemById = () => itemService.getItemById(itemId);
+    const result = getItemById();
+
+    //then
+    expect(getItemByIdMock).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(validItem);
+});
+
+test('given an invalid item id, when an item is requested by id, then an error is thrown', () => {
+    //given
+    itemDb.getItemById = getItemByIdMock.mockReturnValue(null);
+    const itemId = 4;
+    
+    //when
+    const getItem = () => itemService.getItemById(itemId);
+
+    //then
+    expect(getItem).toThrow("No item found for this id");
+});
---
+++ back-end/test/service/profile.service.test.ts
@@ -0,0 +1,73 @@
+import { Category } from "../../model/Category";
+import { Item } from "../../model/Item";
+import { LocationTag } from "../../model/LocationTag";
+import { Profile } from "../../model/Profile";
+import itemDb from "../../repository/item.db";
+import profileDb from "../../repository/profile.db";
+import itemService from "../../service/item.service";
+import profileService from "../../service/profile.service";
+
+
+const validUsername: string = "Michiel05";
+const validUsername2: string = "Kevin04";
+
+const validPassword: string = "K5/#G6es:M(z8,";
+const validPassword2: string = "K4/#G6es:M(z8,";
+
+const validEmail2: string = "Kevin.Hiers@domain.be";
+const validEmail: string = "Michiel.Nijs@domain.be";
+
+const validPhoneNumber: string = "0467725913";
+const validPhoneNumber2: string = "0467724913";
+
+const validLocation: LocationTag = new LocationTag({
+    displayName: "Leuven",
+    latitude: 50.8775,
+    longtitude: 4.70444
+})
+
+const validLocation2: LocationTag = new LocationTag({
+    displayName: "Brussel",
+    latitude: 50.84667,
+    longtitude: 4.35472
+})
+
+const validProfile: Profile = new Profile({id: 1, username: validUsername, password: validPassword, email: validEmail, phoneNumber: validPhoneNumber, location: validLocation});
+const validProfile2: Profile = new Profile({id: 2, username: validUsername2, password: validPassword2, email: validEmail2, phoneNumber: validPhoneNumber2, location: validLocation2});
+
+
+let getProfileByEmailMock: jest.Mock;
+
+beforeEach(() => {
+    getProfileByEmailMock = jest.fn();
+});
+
+afterEach(() => {
+    jest.clearAllMocks();
+});
+
+test('given a valid profile email, when a profile is requested by email, then the profile with that email is returned', () => {
+    //given
+    profileDb.getProfileByEmail = getProfileByEmailMock.mockReturnValue(validProfile);
+    const profileEmail = "Kevin.Hiers@domain.be";
+    
+    //when
+    const getProfileByEmail = () => profileService.getProfileByEmail(profileEmail);
+    const result = getProfileByEmail();
+
+    //then
+    expect(getProfileByEmailMock).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(validProfile);
+});
+
+test('given an invalid profile email, when a profile is requested by email, then an error is thrown', () => {
+    //given
+    profileDb.getProfileByEmail = getProfileByEmailMock.mockReturnValue(null);
+    const profileEmail = "notanemail";
+    
+    //when
+    const getProfile = () => profileService.getProfileByEmail(profileEmail);
+
+    //then
+    expect(getProfile).toThrow("No profile found for this email");
+});
