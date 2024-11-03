02160144121fe2946278df4d629b53b8f68bbf75
AmNotOnline <michiel5.nijs@gmail.com>
2024-11-03 17:03:41 +0100

    Fixed typo

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -37,7 +37,7 @@ export default function Home() {
     <>
       <Head>
         <title>Lenderr</title>
-        <meta name="description" content="The lenderr webiste" />
+        <meta name="description" content="The lenderr website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/Lenderr.png" />
       </Head>
