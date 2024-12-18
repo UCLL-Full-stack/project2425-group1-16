09f9276f82da878120eeeaa7b6070daa87968390
Michiel <michiel5.nijs@gmail.com>
2024-12-18 14:02:11 +0100

    Updated seed file to push straight to DB

--- back-end/util/seed.ts
+++ back-end/util/seed.ts
@@ -1,18 +1,86 @@
-import { Category } from "../model/category";
-import { LocationTag } from "../model/locationTag";
+import { PrismaClient } from '@prisma/client';
 
-const locationLeuven = new LocationTag({
-    displayName: "Martelarenplein, Leuven",
-    longitude: 50.8811884,
-    latitude: 4.7144443,
-});
+const prisma = new PrismaClient();
 
-const locationHeist = new LocationTag({
-    displayName: "Kerkplein, Heist-op-den-Berg",
-    longitude: 51.0761674,
-    latitude: 4.7289136
-});
+const main = async () => {
+    const locationLeuven = await prisma.locationTag.create({
+        data: {
+            displayName: "Martelarenplein, Leuven",
+            longitude: 50.8811884,
+            latitude: 4.7144443,
+        }
+    });
 
-const categoryElectronics = new Category({ name: 'Electronics' });
-const categoryLamps       = new Category({ name: 'Lamps', parents: [categoryElectronics] });
-const categoryGardening   = new Category({ name: 'Gardening' });
+    const locationHeist = await prisma.locationTag.create({
+        data: {
+            displayName: "Kerkplein, Heist-op-den-Berg",
+            longitude: 51.0761674,
+            latitude: 4.7289136
+        }
+    });
+
+    const noLocation = await prisma.locationTag.create({
+        data: {
+            displayName: "No location",
+            longitude: 0.0,
+            latitude: 0.0
+        }
+    });
+
+
+    const categoryElectronics = await prisma.category.create({ data: { name: 'Electronics' } });
+    const categoryLamps       = await prisma.category.create(
+        { data: {
+            name: 'Lamps', 
+            parents: {
+                connect: { id: categoryElectronics.id }
+            }
+        }
+    });
+    const categoryGardening   = await prisma.category.create({ data: { name: 'Gardening' } });
+
+
+    const user = await prisma.profile.create({
+        data: {
+            username: 'exampleUser',
+            password: 'examplePassword',
+            email: 'example@example.com',
+            phoneNumber: '1234567890',
+            role: 'USER',
+            locationTag: { connect: { id: locationLeuven.id } }
+        }
+    })
+
+    const admin = await prisma.profile.create({
+        data: {
+            username: 'exampleAdmin',
+            password: 'examplePassword',
+            email: 'example@example.com',
+            phoneNumber: '1234567890',
+            role: 'ADMIN',
+            locationTag: { connect: { id: locationHeist.id } }
+        }
+    });
+
+    const superAdmin = await prisma.profile.create({
+        data: {
+            username: 'exampleSuperAdmin',
+            password: 'examplePassword',
+            email: 'example@example.com',
+            phoneNumber: '1234567890',
+            role: 'SUPERADMIN',
+            locationTag: { connect: { id: noLocation.id } }
+        }
+    });
+}
+
+(async () => {
+    try {
+        await main();
+        await prisma.$disconnect();
+    } catch (error) {
+        console.error(error);
+        await prisma.$disconnect();
+        process.exit(1);
+    }
+})();
