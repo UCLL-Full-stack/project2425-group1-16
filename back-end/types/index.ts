daafeb7848d5e57c88ea434b246b6c7e1a8b9d60
Michiel <michiel5.nijs@gmail.com>
2024-12-18 02:36:49 +0100

    Added DTOs

--- back-end/types/index.ts
+++ back-end/types/index.ts
@@ -1,3 +1,51 @@
-type Role = 'USER' | 'ADMIN' | 'SUPERADMIN';
+export type Role = 'USER' | 'ADMIN' | 'SUPERADMIN';
+export type LoanState = 'PENDING' | 'ACCEPTED' | 'DENIED';
 
-export { Role };
+export type AuthenticationResponse = {
+    token: string,
+    userId: number,
+    role: Role
+};
+
+export type LocationTagInput = {
+    id?: number 
+    displayName: string,
+    longitude: number,
+    latitude: number,
+};
+
+export type CategoryInput = {
+    id?: number 
+    name: string,
+    parents: CategoryInput[],
+    children: CategoryInput[],
+};
+
+export type ProfileInput = {
+    id?: number,
+    username: string,
+    password: string,
+    email: string,
+    phoneNumber: string,
+    role: Role,
+    locationTag: LocationTagInput,
+};
+
+export type ItemInput = {
+    id?: number,
+    name: string,
+    description: string,
+    price: number,
+    locationTag: LocationTagInput,
+    owner: ProfileInput,
+    categories: CategoryInput[],
+}
+
+export type LoanInput = {
+    id?: number,
+    start: Date,
+    end: Date,
+    state: LoanState,
+    loanedItem: ItemInput;
+    loaner: ProfileInput;
+};
