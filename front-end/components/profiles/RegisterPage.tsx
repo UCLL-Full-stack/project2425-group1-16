c2f196afe3e5fd58ad135bcd9360d967e108832f
Kevin Hiers <kevin.hiers@student.ucll.be>
2024-12-20 22:25:01 +0100

    form validation

--- front-end/components/profiles/RegisterPage.tsx
+++ front-end/components/profiles/RegisterPage.tsx
@@ -10,11 +10,35 @@ const RegisterPage: React.FC = () => {
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
     const [locationDisplayName, setLocationDisplayName] = useState<string>("");
+    const [nameError, setNameError] = useState<String|null>(null);
+    const [emailError, setEmailError] = useState<String|null>(null);
+    const [passwordError, setPasswordError] = useState<String|null>(null);
+    const [locationError, setLocationError] = useState<String|null>(null);
     const [tokenObj, setTokenObj] = useState<TokenObj|null>(null);
     const router = useRouter();
 
+    const resetErrors = () => {
+        setNameError(null);
+        setEmailError(null);
+        setPasswordError(null);
+        setLocationError(null);
+    }
+
     const registerProfile = async (event: React.FormEvent) => {
         event?.preventDefault();
+        resetErrors()
+        if (!name) {
+            setNameError("Invalid username");
+        }
+        if (!email) {
+            setEmailError("Invalid email");
+        }
+        if (!password) {
+            setPasswordError("Invalid password");
+        }
+        if (!locationDisplayName) {
+            setLocationError("Invalid Location");
+        }
         try {
             const response = await ProfileService.register({username: name, email: email, password: password, locationDisplayName: locationDisplayName });
             if (response.status == 200) {
@@ -37,18 +61,22 @@ const RegisterPage: React.FC = () => {
             <div>
                 <p>Username</p>
                 <input type="text" onChange={text => setName(text.target.value)}/>
+                <p style={{color: "#FF0000"}}>{nameError}</p>
             </div>
             <div>
                 <p>Email</p>
                 <input type="text" onChange={text => setEmail(text.target.value)}/>
+                <p style={{color: "#FF0000"}}>{emailError}</p>
             </div>
             <div>
                 <p>Password</p>
                 <input type="text" onChange={text => setPassword(text.target.value)}/>
+                <p style={{color: "#FF0000"}}>{passwordError}</p>
             </div>
             <div>
                 <p>Location</p>
                 <input type="text" onChange={text => setLocationDisplayName(text.target.value)}/>
+                <p style={{color: "#FF0000"}}>{locationError}</p>
             </div>
             <button type="submit">Register</button>
         </form>
